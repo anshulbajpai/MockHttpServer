a76168cae8d285ac4c7b2b43ef2a87e046674f22
Anshul Bajpai <bajpai.anshul@gmail.com>
2012-12-08 18:03:45 +0000

    Refactoring

--- src/test/scala/org/mockhttpserver/MockHttpServerSpec.scala
+++ src/test/scala/org/mockhttpserver/MockHttpServerSpec.scala
@@ -10,20 +10,21 @@ class MockHttpServerSpec extends BddSpec with RestSupport with JsonSupport{
   object RequestEntity extends Entity("Request", 10)
   object ResponseEntity extends Entity("Response", 10)
 
-  def baseUri = "http://localhost:8080/"
   describe("Mock Http Server"){
+    def baseUri = "http://localhost:8080/"
 
     var server : MockHttpServer = null
 
-    it("satisfy the get expectations"){
-      server = MockHttpServer("localhost",8080) {
-        GET("/foo") -> Response(200, Some(PlainText("foo me")))
-      }.start
 
-      val response = jerseyClient.resource(baseUri + "foo").get(classOf[ClientResponse])
-      response.getStatus should  be(200)
-      response.getType should be(MediaType.TEXT_PLAIN_TYPE)
-      response.getEntity(classOf[String]) should be("foo me")
+    it("satisfy the get expectations"){
+
+      server = MockHttpServer("localhost",8080)(
+        GET("/foo") -> Response(200, Some(PlainText("foo me")))
+      ).start
+
+      verifyResponse(200, MediaType.TEXT_PLAIN_TYPE, "foo me"){
+        jerseyClient.resource(baseUri + "foo").get(classOf[ClientResponse])
+      }
     }
 
     it("satisfy the post expectations with Json Request"){
@@ -31,10 +32,9 @@ class MockHttpServerSpec extends BddSpec with RestSupport with JsonSupport{
         POST("/foo", Some(Json(toJson(RequestEntity)))) -> Response(200, Some(Json(toJson(ResponseEntity))))
       }.start
 
-      val response = jerseyClient.resource(baseUri + "foo").`type`("application/json").post(classOf[ClientResponse], toJson(RequestEntity))
-      response.getStatus should  be(200)
-      response.getType should be(MediaType.APPLICATION_JSON_TYPE)
-      response.getEntity(classOf[String]) should be(toJson(ResponseEntity))
+      verifyResponse(200, MediaType.APPLICATION_JSON_TYPE, toJson(ResponseEntity)){
+        jerseyClient.resource(baseUri + "foo").`type`("application/json").post(classOf[ClientResponse], toJson(RequestEntity))
+      }
     }
 
     it("returns 404 if the expectations is not satisfied"){
@@ -43,16 +43,19 @@ class MockHttpServerSpec extends BddSpec with RestSupport with JsonSupport{
         POST("/foo", Some(Json(toJson(RequestEntity)))) -> Response(200, Some(Json(toJson(ResponseEntity))))
       }.start
 
-      val response = jerseyClient.resource(baseUri + "foo").get(classOf[ClientResponse])
-      response.getStatus should  be(404)
-      response.getType should be(MediaType.TEXT_PLAIN_TYPE)
-      response.getEntity(classOf[String]) should be("Request was not matched GET localhost:8080/foo")
+      verifyResponse(404, MediaType.TEXT_PLAIN_TYPE, "Request was not matched GET localhost:8080/foo"){
+        jerseyClient.resource(baseUri + "foo").get(classOf[ClientResponse])
+      }
+    }
+
+    def verifyResponse(status : Int, `type` : MediaType, entity : String)(response: ClientResponse) {
+      response.getStatus should be(status)
+      response.getType should be(`type`)
+      response.getEntity(classOf[String]) should be(entity)
     }
 
     after{
       server.stop
     }
   }
-
-
 }

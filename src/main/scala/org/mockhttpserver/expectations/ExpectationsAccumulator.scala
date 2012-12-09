f73166357c72e6e5fd7f32523b08b049d0aa93c2
Anshul Bajpai <bajpai.anshul@gmail.com>
2012-12-09 18:42:32 +0000

    Refactored Expectations Accumulator

--- src/main/scala/org/mockhttpserver/expectations/ExpectationsAccumulator.scala
+++ src/main/scala/org/mockhttpserver/expectations/ExpectationsAccumulator.scala
@@ -5,15 +5,16 @@ import org.mockhttpserver.core._
 
 class ExpectationsAccumulator(extractor : ExpectationsExtractor) {
 
-  type Transform = ((Request, Response)) => (Request, Response)
+  type ReqRep = (Request, Response)
+  type Transform = (ReqRep) => ReqRep
   private def extract(path : String)(transform : Transform) = extractor.collect(path).map(transform)
 
-  private def extractGet(basePath : String) = extract(basePath + "/get.expectations"){ e => (GET(e._1.url), e._2) }
-  private def extractPost(basePath : String) = extract(basePath + "/post.expectations"){ e => (POST(e._1.url, e._1.body), e._2) }
-  private def extractPut(basePath : String) = extract(basePath + "/put.expectations"){ e => (PUT(e._1.url, e._1.body), e._2) }
-  private def extractDelete(basePath : String) = extract(basePath + "/delete.expectations"){ e => (DELETE(e._1.url), e._2) }
+  private def extractGet= {s : String => extract(s + "/get.expectations"){ e => (GET(e._1.url), e._2)}}
+  private def extractPost = {s : String => extract(s + "/post.expectations"){ e => (POST(e._1.url, e._1.body), e._2)}}
+  private def extractPut = {s : String => extract(s+ "/put.expectations"){ e => (PUT(e._1.url, e._1.body), e._2) }}
+  private val extractDelete = {s : String => extract(s + "/delete.expectations"){e => (DELETE(e._1.url), e._2)}}
 
-  private val extractAll = List(extractGet _, extractPost _, extractPut _, extractDelete _)
+  private val extractAll = List(extractGet, extractPost, extractPut, extractDelete)
 
-  def accumulateFrom (basePath : String) = extractAll.foldRight(List.empty[(Request, Response)]){ _(basePath) ::: _ }
+  def accumulateFrom (basePath : String) = extractAll.foldRight(List.empty[ReqRep]){ _(basePath) ::: _ }
 }

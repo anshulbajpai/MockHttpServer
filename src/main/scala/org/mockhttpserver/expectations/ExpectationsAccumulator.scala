c2d192e221c5160d83fe22ecfc5efcbb3d5ec656
Anshul Bajpai <bajpai.anshul@gmail.com>
2012-12-22 22:59:30 +0000

    Refactored Accumulator

--- src/main/scala/org/mockhttpserver/expectations/ExpectationsAccumulator.scala
+++ src/main/scala/org/mockhttpserver/expectations/ExpectationsAccumulator.scala
@@ -5,12 +5,14 @@ import org.mockhttpserver.expectations.Transform._
 
 class ExpectationsAccumulator(extractor : ExpectationsExtractor) {
 
-  private def extract[T <: Request](path : String)(implicit transform : Transform[T])  = extractor.collect[T](path)
+  private def extract[T <: Request](path : String)(implicit basePath : String, transform : Transform[T])  = {
+    extractor.collect[T](basePath + path)
+  }
 
-  private val extractGet = {s : String => extract[Get](s + "/get.expectations")}
-  private val extractPost = {s : String => extract[Post](s + "/post.expectations")}
-  private val extractPut = {s : String => extract[Put](s+ "/put.expectations")}
-  private val extractDelete = {s : String => extract[Delete](s + "/delete.expectations")}
+  private val extractGet = {implicit s : String => extract[Get]("/get.expectations")}
+  private val extractPost = {implicit s : String => extract[Post]("/post.expectations")}
+  private val extractPut = {implicit s : String => extract[Put]("/put.expectations")}
+  private val extractDelete = {implicit s : String => extract[Delete]("/delete.expectations")}
 
   private val extractAll = List(extractGet, extractPost, extractPut, extractDelete)
 

23eb0e2179f3683f164d708e24d05b5cd16a3ccc
Anshul Bajpai <bajpai.anshul@gmail.com>
2012-12-29 01:21:24 +0000

    Changed the version from usage

--- src/main/scala/org/mockhttpserver/cli/Conf.scala
+++ src/main/scala/org/mockhttpserver/cli/Conf.scala
@@ -6,15 +6,15 @@ class Conf(args : Array[String]) extends ScallopConf(args){
 
     def this(args : String) = this(args.split(" "))
 
-    version("MockHttpServer version 1.0.0")
+    version("MockHttpServer version 1.0")
     banner(
       """
-        |Usage: mock-http-server [OPTION...]
+        |Usage: java -jar MockHttpServer-standalone.jar [OPTION...]
         |Options:
       """.stripMargin)
     footer("For rest, consult the documentation or the developer.")
 
-    val port = opt[Int]("port", default = Some(8080), descr = "port to start the server")
-    val basePath = opt[String]("basePath", required = true, descr = "base path to expectations root folder")
+    val port = opt[Int]("port", default = Some(8080), descr = "(port to start the server)")
+    val basePath = opt[String]("basePath", required = true, descr = "(base path to expectations root folder)")
     val host = "localhost"
 }
